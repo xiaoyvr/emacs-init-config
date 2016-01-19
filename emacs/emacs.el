89ce2427287a6ecf0c5f7a5b9c607711027dee55
xiaoyvr <xiaoyvr@gmail.com>
2016-01-20 00:13:24 +0800

    add more customization

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -4,7 +4,16 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
- '(golden-ratio-mode t))
+ '(global-linum-mode t)
+ '(golden-ratio-mode t)
+ '(package-archives
+   (quote
+    (("gnu" . "http://eLpa.gnu.org/packages/")
+     ("ELPA" . "http://tromey.com/elpa/")
+     ("melpa" . "http://melpa.org/packages/")
+     ("marmalade" . "http://marmalade-repo.org/packages/")
+     ("SC" . "http://joseito.republika.pl/sunrise-commander/")
+     ("org" . "http://orgmode.org/elpa/")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -32,10 +41,17 @@
 
 (setq my-packages
       (append
-       '(cssh el-get switch-window vkill google-maps xcscope yasnippet golden-ratio)
+       '(cssh el-get package switch-window vkill golden-ratio osx-clipboard htmlize linum-ex markdown-mode org-plus-contrib)
        (mapcar 'el-get-source-name el-get-sources)))
 
 (el-get-cleanup my-packages)
 (el-get 'sync my-packages)
 
-(global-set-key "\C-cc" 'org-capture)
+(global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c b") 'org-iswitchb)
+;; (global-set-key (kbd "C-x o") 'switch-window)
+
+(golden-ratio-mode t)
+
